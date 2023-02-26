177962e845d94e29e1c0a73b55b50f3ef8481dbc
Xander Bailey <zander181@gmail.com>
2023-02-26 17:22:41 +0000

    v1

---
+++ my-project/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import AboutMe from "../components/aboutMe";
+
+
+export default function AboutMePage() {
+    return (<div className="space-y-14 lg:space-y-24">
+    <main className="max-w-4xl mx-auto antialiased">
+      <AboutMe/>
+    </main>
+  </div>)
+}
