bd9afc5fc4af3be50e1afb9acb1e0cf9fc1f6b09
Xander Bailey <zander181@gmail.com>
2023-02-26 20:33:42 +0000

    remove header

--- my-project/src/pages/_app.tsx
+++ my-project/src/pages/_app.tsx
@@ -5,7 +5,6 @@ import type { AppProps } from 'next/app'
 
 function MyApp({Component, pageProps}: AppProps) {
   return (<>
-            <Navigation></Navigation>
             <Component {...pageProps} />
       </>)
 }
